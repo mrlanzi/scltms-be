a61e9d05b8bac14bffa1d75ce852fa076984bd0e
MidKnight <midknight@obsidian-ware.com>
2024-10-19 16:11:30 +0200

    Project init

---
+++ src/main/java/com/obsidianware/schooltimes/scltmsbe/ScltmsbeApplication.java
@@ -0,0 +1,13 @@
+package com.obsidianware.schooltimes.scltmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScltmsbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScltmsbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obsidianware/schooltimer/scltmrbe/ScltmrbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obsidianware.schooltimer.scltmrbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScltmrbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
